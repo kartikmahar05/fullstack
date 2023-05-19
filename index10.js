5a98981e33a5837d2ec7180791c7fd004ddcd392
kartikmahar05 <kartikmahar5@gmail.com>
2023-05-19 18:09:09 +0530

    calculator

---
+++ index10.js
@@ -0,0 +1,21 @@
+function add(){
+let a=parseInt(prompt("Enter the no: "))
+let b=parseInt(prompt("Enter the no: "))
+    document.write(a+b,"<br>");
+}
+function Sub(){
+    let a=parseInt(prompt("Enter the no: "))
+    let b=parseInt(prompt("Enter the no: "))
+    document.write(a-b,"<br>");
+}
+function multiply(){
+    let a=parseInt(prompt("Enter the no: "))
+    let b=parseInt(prompt("Enter the no: "))
+    document.write(a*b,"<br>");
+}
+function divide(){
+    let a=parseInt(prompt("Enter the no: "))
+    let b=parseInt(prompt("Enter the no: "))
+    document.write(a/b,"<br>");
+}
+
